1e5b758fd9085ae9179def97b96310206a0bb287
Craig Jennings <craig.jennings11@gmail.com>
2019-07-18 14:27:31 -0500

    Split base styles into separate definitions

---
+++ src/styles/base/unstyled.js
@@ -0,0 +1,13 @@
+import { css } from 'lit-element';
+
+export default css`
+  a.unstyled {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  a.unstyled:hover,
+  a.unstyled:visited {
+    text-decoration: none;
+  }
+`;
