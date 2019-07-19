1467051f922ee8c060cf4f6a4daed05578397492
Craig Jennings <craig.jennings11@gmail.com>
2019-07-18 20:19:19 -0500

    Simplify service worker update notification

--- src/service-worker/sw-installer.js
+++ src/service-worker/sw-installer.js
@@ -4,20 +4,14 @@ import { dispatch } from '../store.js';
 import { html } from 'lit-html';
 import { Workbox } from 'workbox-window';
 
-const updateAvailable = new Promise((resolve) => {
-  if ('serviceWorker' in navigator) {
-    const wb = new Workbox('./service-worker.js');
+if ('serviceWorker' in navigator) {
+  const wb = new Workbox('./service-worker.js');
 
-    wb.addEventListener('message', (event) => {
-      if (event.data.type === 'CACHE_UPDATED') {
-        resolve();
-      }
-    });
+  wb.addEventListener('message', (event) => {
+    if (event.data.type === 'CACHE_UPDATED') {
+      dispatch(addNotification(html`<ba-sw-update></ba-sw-update>`));
+    }
+  });
 
-    wb.register();
-  }
-});
-
-updateAvailable.then(() => {
-  dispatch(addNotification(html`<ba-sw-update></ba-sw-update>`));
-});
+  wb.register();
+}
