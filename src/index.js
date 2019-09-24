7a09c61744db52ee319fb9d86a6ca880427e9572
Craig Jennings <craig.jennings11@gmail.com>
2019-09-24 12:33:51 -0500

    Pick up where user left off

--- src/index.js
+++ src/index.js
@@ -10,14 +10,29 @@ import {
 } from './pages.js';
 import page from 'page';
 
+const LS_PATH_KEY = 'current:path';
+
 const render = (ctx) => {
   document.querySelector('ba-shell').template = ctx.template;
 };
 
+const storeCurrentLocation = (ctx, next) => {
+  localStorage.setItem(LS_PATH_KEY, ctx.path);
+
+  next();
+};
+
+page('*', storeCurrentLocation);
 page('/', bookSelectorPage, render);
 page('/search', searchPage, render);
 page('/:book', chapterSelectorPage, render);
 page('/:book/:chapter', passagePage, render);
 page('*', unknownPage, render);
+
+const currentPath = localStorage.getItem(LS_PATH_KEY);
+
+if (currentPath) {
+  page.redirect(currentPath);
+}
 
 page.start();
