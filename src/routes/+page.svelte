681c03d14b91471b1f15478fa0bee4eaab0f37b8
Craig Jennings <1683368+craig-jennings@users.noreply.github.com>
2024-12-06 14:49:12 -0600

    fix: adjust maskable icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,44 @@
 <script lang="ts">
-	import Book from '$lib/components/Book.svelte';
+	import Input from '$lib/components/common/Input.svelte';
+	import SelectorList from '$lib/components/SelectorList.svelte';
+	import SelectorListItem from '$lib/components/SelectorListItem.svelte';
+	import { NewTestament } from '$lib/constants/NewTestament';
+	import { OldTestament } from '$lib/constants/OldTestament';
+	import searchBooks from '$lib/utils/searchBooks';
+
+	/* -- Runes -- */
+	let term = $state('');
+
+	let newTestamentBooks = $derived(searchBooks(NewTestament, term.toLowerCase()));
+	let oldTestamentBooks = $derived(searchBooks(OldTestament, term.toLowerCase()));
+
+	/* -- Event Handlers -- */
 </script>
 
-<Book />
+<Input
+	aria-label="Book Search Field"
+	class="mb-8 w-full"
+	oninput={(e) => (term = e.currentTarget.value)}
+	placeholder="Find Book..."
+	value={term}
+/>
+
+{#if oldTestamentBooks.length > 0}
+	<h1 class="mb-6 text-center text-3xl font-bold">Old Testament</h1>
+{/if}
+
+<SelectorList>
+	{#each oldTestamentBooks as otBook}
+		<SelectorListItem href="/{otBook.key}">{otBook.label}</SelectorListItem>
+	{/each}
+</SelectorList>
+
+{#if newTestamentBooks.length > 0}
+	<h1 class="mb-6 mt-8 text-center text-3xl font-bold">New Testament</h1>
+{/if}
+
+<SelectorList>
+	{#each newTestamentBooks as ntBook}
+		<SelectorListItem href="/{ntBook.key}">{ntBook.label}</SelectorListItem>
+	{/each}
+</SelectorList>
--- src/routes/[bookId]/+page.svelte
+++ src/routes/[bookId]/+page.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import Chapter from '$lib/components/Chapter.svelte';
+	import SelectorList from '$lib/components/SelectorList.svelte';
+	import SelectorItem from '$lib/components/SelectorListItem.svelte';
+	import { findBook } from '$lib/utils/findBook';
 
 	/* -- Runes -- */
+	let book = $derived(findBook($page.params.bookId));
+
 	/* -- Event Handlers -- */
 </script>
 
-<Chapter book={$page.params.bookId} />
+{#if book.chapterCount === 0}
+	<div class="text-center">Unknown book</div>
+{:else}
+	<div class="p-4">
+		<SelectorList>
+			<!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
+			{#each Array(book.chapterCount) as _, index}
+				<SelectorItem href="/{$page.params.bookId}/{index + 1}">
+					{index + 1}
+				</SelectorItem>
+			{/each}
+		</SelectorList>
+	</div>
+{/if}
